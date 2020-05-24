31531ed89b46fbf3c9b9ca5a38fad67856cbf663
Markus Engsner <markus.engsner@gmail.com>
2020-05-24 08:41:21 +0200

    Add line PoC

---
+++ line_follower_test/line_follower_test.ino
@@ -0,0 +1,18 @@
+#define RIGHT_LINE_PIN 0
+#define LEFT_LINE_PIN 1
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int right_line = analogRead(RIGHT_LINE_PIN);
+  int left_line = analogRead(LEFT_LINE_PIN);
+  Serial.print(right_line);
+  Serial.print(",");              //seperator
+  Serial.println(left_line);
+  delay(100);
+
+}
